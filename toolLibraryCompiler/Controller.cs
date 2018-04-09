d93ec97f6a14b835fe077f3b5934abc762cc7296
Philippe <philippe.meyer@hotmail.be>
2018-04-09 21:18:06 +0200

    dwadw

--- toolLibraryCompiler/Controller.cs
+++ toolLibraryCompiler/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,49 @@ namespace toolLibraryCompiler
     class Controller
     {
         private List<Tool> Tools = new List<Tool>();
+        private DataSet ToolSet;
+        private DataTable ActiveTable;
 
-        
+        public void LoadSheet(DataSet dataSet)
+        {
+            this.ToolSet = dataSet;           
+        }
+
+        public void SetActiveSheet(DataTable table)
+        {
+            this.ActiveTable = table;
+        }
+
+        public string ActiveSheetToLibrary()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(DataRow row in ActiveTable.Rows)
+            {
+                string type = row.Field<string>("ToolType");
+                switch (type)
+                {
+                    case "BallNose":
+                        BallNose ballnose = new BallNose(row.Field<string>("Name"), row.Field<string>("ToolType"), row.Field<string>("Units"), row.Field<string>("D1"), row.Field<string>("ToolType"))
+                        break;
+                    case "BullNose":
+                        break;
+                    case "Conic":
+                        break;
+                    case "DrillBit":
+                        break;
+                    case "EndMill":
+                        break;
+                    case "Engrave":
+                        break;
+                    case "Engrave2":
+                        break;
+                    case "Spherical":
+                        break;
+                    case "Taper":
+                        break;
+                }
+            }
+        }
     }
 }
